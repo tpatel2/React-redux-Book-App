50b470071bd263bffe387eff6a52180f86cfb6ee
Tapas Patel <tapas.patel92@gmail.com>
2018-04-06 12:06:03 -0400

    react book select app using redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Javascript : The real code', pages: 400},
+        {title: 'ReactJs: redux', pages: 250},
+        {title: 'java: backend Technology', pages: 800},
+        {title: '.net: language', pages: 500}
+        ]
+}
